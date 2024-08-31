f10d6a811bb7c7b7dc092d77a575a99455448241
darvinm11 <dmarroquiny@miumg.edu.gt>
2024-08-31 11:58:43 -0600

    Git: Commit Staged (Amend)

---
+++ Ejemplo de Listas en python.py
@@ -0,0 +1,27 @@
+#Ejemplo 1 Creacion de listas 
+#serpList = list(("Boa", "Pitón", "Culebra venenosa", "Víbora Rayada"))
+#print(serpList) 
+
+
+#Ejemplo 2 Uso de Operadores
+#Suma o concatenar (+)
+#serpList = ["Boa", "Pitón", "Víbora Rayada"]
+#print(serpList + ["182 centímetros", "121 centímetros", "30 centímetros"])
+
+#Rango o slice
+#print(serpList[0:4])
+
+#Multiplicacion o repetir (*)
+#serpList = ["Boa", "Pitón", "Víbora Rayada"]serpList = serpList * 3
+#print(serpList)
+
+#Metodos de listas en python
+
+#.apend 
+#serpList = ["Boa", "Pitón", "Víbora Rayada"]serpList.append("Cobra")
+
+#index
+#serpList.index("Cobra")
+
+#remove
+#serpList.remove("Cobra")
---
+++ Python/20.py
@@ -0,0 +1,12 @@
+num1 = 10
+num2 = 14
+num3 = 17
+
+if (num1 >= num2) and (num1 >= num3):
+    largest = num1
+elif (num2 >= num1) and (num2 >= 3):
+    largest = num2
+else:
+    largest = num3
+
+print("The largest number is", largest)
